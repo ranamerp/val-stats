1bdf13bcb72d6c070289a134ccdc624d68ed56c6
Prem Rana <ranamerp@gmail.com>
2024-11-12 11:06:44 -0600

    Removed extra comment

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,36 +1,3 @@
-// import { agents } from "../../stores/Agents.js";
-//import { getMatchData } from '../../lib/match.js'
-
-// async function getMatchData(region, name, tag) {
-//     const url = `https://api.henrikdev.xyz/valorant/v3/matches/${region}/${name}/${tag}?mode=custom`;
-//     try {
-//         const response = await fetch(url, {
-//           method: 'GET',
-//           headers: {
-//             'Authorization': 'HDEV-de097c2b-bc59-4f35-a19b-f9308d212407',
-//           },
-//         });
-    
-//         if (!response.ok) {
-//           throw new Error(`API request failed with status ${response.status}`);
-//         }
-    
-//         const data = await response.json();
-
-//         if (!data.data || data.data.length === 0) {
-//             //This eventually needs to be a popup sent back to the user
-//             console.warn('No matching data found.')
-//             return null;
-//         }
-
-//         return data.data;
-//     } catch (error) {
-//         console.error('Error fetching match data:', error);
-//         return null;
-//     }
-// }    
-
-
 export async function load({ fetch }) {
     const region = 'na';
     let name = 'kaoticcc';
