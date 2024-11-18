32ecc89e5d3d833affc652f4c0328c293b73ad72
Prem Rana <ranamerp@gmail.com>
2024-11-17 21:24:27 -0600

    Fixed output route

---
+++ src/routes/output/+page.svelte
@@ -0,0 +1,13 @@
+<script lang='ts'>
+import Stats from "../../components/Stats.svelte";
+import { selectedmatch } from "../../stores/SelectedMatch"
+
+// TODO: Add return button, make sure stats is cached or soemthing liek that.
+</script>
+
+<div> 
+   <Stats 
+      playerData={$selectedmatch.match}
+      colors = {$selectedmatch.colors}
+   />
+</div>
--- src/stores/SelectedMatch.ts
+++ src/stores/SelectedMatch.ts
@@ -1,8 +1,9 @@
 //This store should be used to persist the selected match. It'll be referred to in the output route. 
 // I think this can be done better (probably with Supabase), so will aim to remove this eventually
-import { writable } from 'svelte/store'
+import { writable, type Writable } from 'svelte/store'
 
-export const selectedmatch = writable ({
+//I'm going to ignore the TS error here since this is going to be eventually removed
+export const selectedmatch: Writable<{match: App.LocalMatch, colors: App.ColorPreset}> = writable ({
     match: {},
     colors: {}
 })
