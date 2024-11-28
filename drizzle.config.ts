9dea329245af120482a58d2726870bc7215df31c
Prem Rana <ranamerp@gmail.com>
2024-11-28 09:43:29 -0600

    Some extra config file stuff

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,8 +5,13 @@ export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 
 	dbCredentials: {
+		host: "localhost",
+		port: 54321,
+		user: "postgres",
+		password: "postgres",
 		url: process.env.DATABASE_URL
 	},
+	out: "./supabase/migrations",
 
 	verbose: true,
 	strict: true,
