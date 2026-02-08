5a02c28a6ad6c4881aabe0e619e4bafd84949c3b
Prem Rana <ranamerp@gmail.com>
2026-02-07 16:31:03 -0800

    Added ability to get last player on first load

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -4,17 +4,17 @@ import { updateAgents } from '../stores/Agents';
 import { updateMaps } from '../stores/Maps';
 
 
+
 export const load: PageLoad = async ({ fetch, parent }) => {
 
     await updateMaps();
-
     await updateAgents();
 
     const statsPromise = (async () => {
         // Match fetching logic
         const region = 'na';
         let name: string = 'zeek';
-        let puuids: string[] | null = null;
+        let puuids: { puuid: string, regn: string }[] | null = null;
         let final_puuid: string | null = null;
         const tag = '777';
         //Potential improvement: check if user is logged in and get their current match from db. 
@@ -33,7 +33,7 @@ export const load: PageLoad = async ({ fetch, parent }) => {
                 .single();
             
             const playerlist = data?.current_match.red_team.players;
-            puuids = playerlist?.map((p: { puuid: string }) => p.puuid) ?? [];
+            puuids = playerlist?.map((p: { puuid: string, regn: string }) => ({ puuid: p.puuid, regn: data?.current_match.region })) ?? [];
             if (error) {
                 //return { status: 404, error: error.message };
                 //This acts as a fallback
@@ -51,8 +51,8 @@ export const load: PageLoad = async ({ fetch, parent }) => {
         let matchResponse: Response | null = null;
         if (puuids) {
             // Go through it, if 
-            for (const puuid of puuids) {
-                matchResponse = await fetch(`/api/match?region=${region}&puuid=${puuid}`);
+            for (const { puuid, regn } of puuids) {
+                matchResponse = await fetch(`/api/match?region=${regn ?? 'na'}&puuid=${puuid}`);
                 if (matchResponse.status === 200) {
                     final_puuid = puuid;
                     break; // Exit the loop if a successful response is received
@@ -80,7 +80,7 @@ export const load: PageLoad = async ({ fetch, parent }) => {
                 ? [...d[0].red_team.players, ...d[0].blue_team.players]
                 : [];
             const player = players.find(p => p.puuid === final_puuid);
-            name = player ? player.name + '#' + tag : name;
+            name = player ? player.name + '#' + player.tag : name;
             matchData = {status: 200, data: d}
         }
 
